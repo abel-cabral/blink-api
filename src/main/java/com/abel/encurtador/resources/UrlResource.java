8740407410ec47bf082c64641d7ca7bed644abfa
abel-cabral <jcabral@id.uff.br>
2019-07-31 06:18:23 -0300

    Teste de carregamento de pagina

--- src/main/java/com/abel/encurtador/resources/UrlResource.java
+++ src/main/java/com/abel/encurtador/resources/UrlResource.java
@@ -6,31 +6,42 @@ import com.abel.encurtador.services.UrlService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RestController
 public class UrlResource {
     @Autowired
     private UrlService service;
 
-    @PostMapping("/encurtar/{url}")
+    @PostMapping("/encurtador/{url}")
     ResponseEntity<Url> encurteUrl(@PathVariable("url") String fullUrl) {
-        URLShortener uc;
+        URLShortener uc = new URLShortener(5, "https://encurtadorx.herokuapp.com/"); // Tamanho e Dominio
+        String shortUrl;
         Url aux;
 
         // Verifica se o hash gerado tem colisoes
         do {
-            uc = new URLShortener(5, "gool.com/"); // Tamanho e Dominio
+            shortUrl = uc.shortenURL(fullUrl);
             aux = service.findShortUrl(uc.shortenURL(fullUrl));
         } while (aux != null);
 
         // Se nao houveram colisoes chama o salvar
-        aux = service.saveUrl(new Url(null, fullUrl, uc.shortenURL(fullUrl)));
+        aux = service.saveUrl(new Url(null, fullUrl, shortUrl));
         return ResponseEntity.ok().body(aux);
     }
 
-    @GetMapping("/")
-    ResponseEntity<Url> buscarUrl(@RequestBody Url obj) {
-        Url aux = service.findShortUrl(obj.getShortUrl());
-        return ResponseEntity.ok().body(aux);
+    @GetMapping("/{code}")
+    public void buscarUrl(@PathVariable("code") String code, HttpServletResponse redService) {
+        Url aux = service.findShortUrl(code);
+        try {
+            if (aux != null ) {
+                redService.sendRedirect(aux.getLongUrl());
+            }
+        } catch (IOException e) {
+            ResponseEntity.badRequest().body("Error ao consultar");
+        }
     }
 }
